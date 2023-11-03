7fe49f8d11eb5742ffcfc38eb4e194cbf7d0f78b
Ahtisham kiani <ehtisham.alphasquad@gmail.com>
2023-11-04 00:17:23 +0500

    updated

--- src/app/components/candlestickChart/index.js
+++ src/app/components/candlestickChart/index.js
@@ -1,42 +1,65 @@
-import React, { useEffect, useRef } from 'react';
-import { createChart, UTCTimestamp, PriceScaleMode, en } from 'lightweight-charts';
+import React, { useEffect, useRef } from "react";
+import {
+  createChart,
+  UTCTimestamp,
+  PriceScaleMode,
+  en,
+} from "lightweight-charts";
 
-const CandlestickChart = ({ candlestickData }) => {
+const CandlestickChart = ({ parsedMessage }) => {
+  console.log(parsedMessage, "ssssssssssssssssssssss");
   const containerRef = useRef(null);
   let chart = null;
 
   useEffect(() => {
+    if (!parsedMessage || !parsedMessage.data) {
+      return;
+    }
+    const [timeStr, openStr, highStr, lowStr, closeStr] = parsedMessage.data
+      .map((str) => {
+        console.log({ str });
+        return str.map((innerStr) => parseFloat(innerStr));
+      })
+      .flat();
     chart = createChart(containerRef.current, {
-    //   width: '100%', // Set the chart width to 100% to make it responsive
+      // width: "100%", // Set the chart width to 100% to make it responsive
       height: 800,
       localization: en,
     });
-    // function generateRandomDataPoint() {
-    //     const time = Date.now() - Math.floor(Math.random() * 10000); // Generate a random timestamp
-    //     const open = Math.random() * 100 + 50; // Random open price between 50 and 150
-    //     const close = open + Math.random() * 20 - 10; // Random close price within +/- 10 of open
-    //     const high = Math.max(open, close) + Math.random() * 10; // Random high price above open/close
-    //     const low = Math.min(open, close) - Math.random() * 10; // Random low price below open/close
-      
-    //     return {
-    //       time,
-    //       open,
-    //       high,
-    //       low,
-    //       close,
-    //     };
-    //   }
-      
-    //   const generateCandlestickData = (count) => {
-    //     const data = [];
-    //     for (let i = 0; i < count; i++) {
-    //       data.push(generateRandomDataPoint());
-    //     }
-    //     return data;
-    //   };
-      
-    //   const candlestickData = generateCandlestickData(100);
-    //   candlestickData.sort((a, b) => a.time - b.time);
+
+    const generateCandlestickData = (
+      timestamps,
+      opens,
+      closes,
+      highs,
+      lows
+    ) => {
+      const data = [];
+      for (let i = 0; i < timestamps.length; i++) {
+        data.push({
+          time: timestamps[i],
+          open: opens[i],
+          high: highs[i],
+          low: lows[i],
+          close: closes[i],
+        });
+      }
+      console.log(data);
+      return data;
+    };
+
+    const timestamps = [timeStr];
+    const opens = [openStr];
+    const closes = [closeStr];
+    const highs = [highStr];
+    const lows = [lowStr];
+    const candlestickData = generateCandlestickData(
+      timestamps,
+      opens,
+      closes,
+      highs,
+      lows
+    );
     const candlestickSeries = chart.addCandlestickSeries();
 
     candlestickSeries.setData(candlestickData);
@@ -47,6 +70,8 @@ const CandlestickChart = ({ candlestickData }) => {
         mode: PriceScaleMode.Normal,
       },
     });
+    console.log(timeStr, "timeStr");
+    console.log(candlestickData, "----");
 
     // Function to update the chart's dimensions on window resize
     const handleResize = () => {
@@ -56,19 +81,19 @@ const CandlestickChart = ({ candlestickData }) => {
     };
 
     // Attach the resize event listener
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
       // Remove the resize event listener when the component unmounts
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
 
       if (chart) {
         chart.remove();
       }
     };
-  }, [candlestickData]);
+  }, [parsedMessage]);
 
-  return <div className='chart-container' ref={containerRef}></div>;
+  return <div className="chart-container" ref={containerRef}></div>;
 };
 
 export default CandlestickChart;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 "use client";
-import React from "react";
 import CandlestickChart from "./components/candlestickChart";
 import AreaChart from "./components/areaChart";
+import React, { useEffect, useRef, useState } from "react";
+import useWebSocket from "react-use-websocket";
+
 export default function Home() {
   const data = [
     { value: 0, time: 1642425322 },
@@ -28,17 +30,47 @@ export default function Home() {
     { time: 1673280000000, open: 108, high: 115, low: 100, close: 112 }, // Green candle (up trade)
     // Add more static candlestick data points as needed
   ];
+  // WebSocket connection URL
+  const socketUrl = "wss://wspap.okx.com:8443/ws/v5/business?brokerId=9999";
+
+  // The hook returns a send function and the last message received
+  const { sendMessage, lastMessage } = useWebSocket(socketUrl, {
+    onOpen: () => console.log("WebSocket Connected"),
+    // Will attempt to reconnect on all close events
+    shouldReconnect: (closeEvent) => true,
+  });
+
+
+  useEffect(() => {
+    const message = {
+      op: "subscribe",
+      args: [
+        {
+          channel: "mark-price-candle5m",
+          instId: "BTC-USD-SWAP",
+        },
+      ],
+    };
+    sendMessage(JSON.stringify(message));
+  }, [sendMessage]);
+
+  // You can use lastMessage for the most recent message
+  // Use JSON.parse only when lastMessage is not null
+  const parsedMessage = lastMessage ? JSON.parse(lastMessage.data) : null;
+  // console.log(parsedMessage, "parsedMessage");
   return (
     <React.Fragment>
       <div className="w-full h-screen  p-8">
         <p className="text-5xl text-center py-4 w-full font-bold">MT TRADING</p>
         <div className="w-full   flex flex-col md:flex-row  gap-5 ">
           <div className="w-full flex flex-col gap-3  shadow-md bg-slate-50 rounded-sm p-5">
-          <span className="text-3xl text-black font-bold">Candle chart</span>
-          <CandlestickChart candlestickData={candlestickData} />
+            <span className="text-3xl text-black font-bold">Candle chart</span>
+            <CandlestickChart 
+            parsedMessage={parsedMessage}
+            candlestickData={candlestickData} />
           </div>
           <div className="w-full flex flex-col gap-3  shadow-md bg-slate-50 rounded-sm p-5">
-          <span className="text-3xl text-black font-bold">Area chart</span>
+            <span className="text-3xl text-black font-bold">Area chart</span>
             <AreaChart data={data} />
           </div>
         </div>
