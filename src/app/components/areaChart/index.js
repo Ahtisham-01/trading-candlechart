9c8ad7aaaa480186a4785dd35f8d3f0b6b9536ea
Ahtisham kiani <ehtisham.alphasquad@gmail.com>
2023-11-03 05:15:13 +0500

    first commit

--- src/app/components/areaChart/index.js
+++ src/app/components/areaChart/index.js
@@ -1,21 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import { createChart, UTCTimestamp, PriceScaleMode, en } from 'lightweight-charts';
 
-const AreaChart = ({data}) => {
+const AreaChart = ({ data }) => {
   const containerRef = useRef(null);
+  let chart = null;
 
   useEffect(() => {
-    let chart = null;
-    let areaSeries = null;
-
     chart = createChart(containerRef.current, {
-      width: 1200,
+    //   width: '100%',
       height: 800,
       layout: { textColor: 'black', background: { type: 'solid', color: 'white' } },
       localization: en,
     });
 
-    areaSeries = chart.addAreaSeries({
+    const areaSeries = chart.addAreaSeries({
       lineColor: '#2962FF',
       topColor: '#2962FF',
       bottomColor: 'rgba(41, 98, 255, 0.28)',
@@ -28,16 +26,30 @@ const AreaChart = ({data}) => {
 
     areaSeries.setData(areaData);
 
+    // Function to update the chart's dimensions on window resize
+    const handleResize = () => {
+      if (chart) {
+        chart.resize(containerRef.current.clientWidth, 800); // Adjust the height as needed
+      }
+    };
+
+    // Attach the resize event listener
+    window.addEventListener('resize', handleResize);
+
+    // Fit the chart to the container
     chart.timeScale().fitContent();
 
     return () => {
+      // Remove the resize event listener when the component unmounts
+      window.removeEventListener('resize', handleResize);
+
       if (chart) {
         chart.remove();
       }
     };
   }, [data]);
 
-  return <div ref={containerRef}></div>;
+  return <div className='chart-container' ref={containerRef}></div>;
 };
 
 export default AreaChart;
--- src/app/components/candlestickChart/index.js
+++ src/app/components/candlestickChart/index.js
@@ -1,21 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import { createChart, UTCTimestamp, PriceScaleMode, en } from 'lightweight-charts';
 
-const CandlestickChart = ({candlestickData}) => {
+const CandlestickChart = ({ candlestickData }) => {
   const containerRef = useRef(null);
+  let chart = null;
 
   useEffect(() => {
-    let chart = null;
-    let candlestickSeries = null;
-
     chart = createChart(containerRef.current, {
-      width: 1200,
+    //   width: '100%', // Set the chart width to 100% to make it responsive
       height: 800,
       localization: en,
     });
 
-    candlestickSeries = chart.addCandlestickSeries();
-   
+
+      
     // function generateRandomDataPoint() {
     //     const time = Date.now() - Math.floor(Math.random() * 10000); // Generate a random timestamp
     //     const open = Math.random() * 100 + 50; // Random open price between 50 and 150
@@ -42,6 +40,8 @@ const CandlestickChart = ({candlestickData}) => {
       
     //   const candlestickData = generateCandlestickData(100);
     //   candlestickData.sort((a, b) => a.time - b.time);
+    const candlestickSeries = chart.addCandlestickSeries();
+
     candlestickSeries.setData(candlestickData);
 
     // Customize the price scale
@@ -51,14 +51,27 @@ const CandlestickChart = ({candlestickData}) => {
       },
     });
 
+    // Function to update the chart's dimensions on window resize
+    const handleResize = () => {
+      if (chart) {
+        chart.resize(containerRef.current.clientWidth, 800); // Adjust the height as needed
+      }
+    };
+
+    // Attach the resize event listener
+    window.addEventListener('resize', handleResize);
+
     return () => {
+      // Remove the resize event listener when the component unmounts
+      window.removeEventListener('resize', handleResize);
+
       if (chart) {
         chart.remove();
       }
     };
-  }, []);
+  }, [candlestickData]);
 
-  return <div ref={containerRef}></div>;
+  return <div className='chart-container' ref={containerRef}></div>;
 };
 
 export default CandlestickChart;
