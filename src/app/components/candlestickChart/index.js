2f51bb3646874d9ecc2f34a1ab3db50dc4ad3cc2
Ahtisham kiani <ehtisham.alphasquad@gmail.com>
2023-11-07 16:57:18 +0500

    candle chart working fine

--- src/app/components/candlestickChart/index.js
+++ src/app/components/candlestickChart/index.js
@@ -1,96 +1,181 @@
+// import React, { useEffect, useRef, useState } from "react";
+// import { createChart, PriceScaleMode, en } from "lightweight-charts";
+
+// const CandlestickChart = ({ parsedMessage }) => {
+//   const containerRef = useRef(null);
+//   const [chart, setChart] = useState(null);
+//   const [candlestickSeries, setCandlestickSeries] = useState(null);
+//   const [candles, setCandles] = useState([]);
+
+//   useEffect(() => {
+//     const newChart = createChart(containerRef.current, {
+//       width: containerRef.current.clientWidth,
+//       height: 800,
+//       localization: en,
+//     });
+//     setChart(newChart);
+//     const newCandlestickSeries = newChart.addCandlestickSeries({
+//       timeScale: {
+//         timeVisible: true,
+//         secondsVisible: true,
+//       },
+//     });
+//     setCandlestickSeries(newCandlestickSeries);
+
+//     return () => newChart.remove(); // Clean up on component unmount
+//   }, []);
+
+//   useEffect(() => {
+//     if (!parsedMessage || !parsedMessage.data || !candlestickSeries) return;
+//     console.log(parsedMessage.data[0][0], "pppppppppppppppp");
+//     // Parse the new candle from the message
+//     const newCandle = {
+//       time: parseInt(parsedMessage.data[0][0], 10) / 1000, // Convert timestamp to seconds
+//       open: parseFloat(parsedMessage.data[0][1]),
+//       high: parseFloat(parsedMessage.data[0][2]),
+//       low: parseFloat(parsedMessage.data[0][3]),
+//       close: parseFloat(parsedMessage.data[0][4]),
+//     };
+
+//     // Update the candles state, removing duplicates and keeping only the data for the last 10 minutes
+//     setCandles((prevCandles) => {
+//       const currentTime = new Date().getTime() / 1000; // Current time in seconds
+//       const tenMinutesAgo = currentTime - 600; // Time 10 minutes ago in seconds
+
+//       // Remove outdated candles
+//       let updatedCandles = prevCandles.filter(
+//         (candle) => candle.time > tenMinutesAgo
+//       );
+
+//       // Check if the candle with the current timestamp already exists and update it; otherwise, add as a new candle
+//       const existingIndex = updatedCandles.findIndex(
+//         (candle) => candle.time === newCandle.time
+//       );
+//       if (existingIndex >= 0) {
+//         updatedCandles[existingIndex] = newCandle;
+//       } else {
+//         updatedCandles.push(newCandle);
+//       }
+
+//       return updatedCandles;
+//     });
+//   }, [parsedMessage, candlestickSeries]);
+
+//   useEffect(() => {
+//     if (candlestickSeries && candles.length) {
+//       candlestickSeries.setData(candles);
+//     }
+//   }, [candles, candlestickSeries]);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       if (chart) {
+//         chart.resize(containerRef.current.clientWidth, 800);
+//       }
+//     };
+
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, [chart]);
+
+//   return <div className="chart-container" ref={containerRef}></div>;
+// };
+
+// export default CandlestickChart;
+
 import React, { useEffect, useRef } from "react";
-import {
-  createChart,
-  PriceScaleMode,
-  en,
-} from "lightweight-charts";
+import { createChart, PriceScaleMode, en } from "lightweight-charts";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
-const CandlestickChart = ({ parsedMessage }) => {
-  console.log(parsedMessage, "ssssssssssssssssssssss");
+const CandlestickChart = () => {
+  const socketUrl = "wss://wspap.okx.com:8443/ws/v5/business?brokerId=9999";
   const containerRef = useRef(null);
-  let chart = null;
+  const chartRef = useRef(null);
+  const candlestickSeriesRef = useRef(null);
+  const candlesRef = useRef([]);
 
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
+    shouldReconnect: (closeEvent) => true, // Will attempt to reconnect on all close events
+  });
+
+  // Only subscribe once when the WebSocket is first opened
   useEffect(() => {
-    if (!parsedMessage || !parsedMessage.data) {
-      return;
+    if (readyState === ReadyState.OPEN) {
+      console.log("WebSocket Connected");
+      sendMessage(
+        JSON.stringify({
+          op: "subscribe",
+          args: [{ instId: "BTC-USD-SWAP", channel: "mark-price-candle1m" }],
+        })
+      );
     }
-    const [timeStr, openStr, highStr, lowStr, closeStr] = parsedMessage.data
-      .map((str) => {
-        console.log({ str });
-        return str.map((innerStr) => parseFloat(innerStr));
-      })
-      .flat();
-    chart = createChart(containerRef.current, {
-      // width: "100%", // Set the chart width to 100% to make it responsive
+  }, [readyState, sendMessage]);
+
+  // Initialize the chart and series on component mount
+  useEffect(() => {
+    chartRef.current = createChart(containerRef.current, {
+      width: containerRef.current.clientWidth,
       height: 800,
       localization: en,
     });
-
-    const generateCandlestickData = (
-      timestamps,
-      opens,
-      closes,
-      highs,
-      lows
-    ) => {
-      const data = [];
-      for (let i = 0; i < timestamps.length; i++) {
-        data.push({
-          time: timestamps[i],
-          open: opens[i],
-          high: highs[i],
-          low: lows[i],
-          close: closes[i],
-        });
-      }
-      console.log(data);
-      return data;
-    };
-
-    const timestamps = [timeStr];
-    const opens = [openStr];
-    const closes = [closeStr];
-    const highs = [highStr];
-    const lows = [lowStr];
-    const candlestickData = generateCandlestickData(
-      timestamps,
-      opens,
-      closes,
-      highs,
-      lows
-    );
-    const candlestickSeries = chart.addCandlestickSeries();
-
-    candlestickSeries.setData(candlestickData);
-
-    // Customize the price scale
-    chart.applyOptions({
-      priceScale: {
-        mode: PriceScaleMode.Normal,
-      },
+    candlestickSeriesRef.current = chartRef.current.addCandlestickSeries({
+      timeScale: { timeVisible: true, secondsVisible: true },
     });
-    console.log(timeStr, "timeStr");
-    console.log(candlestickData, "----");
-
-    // Function to update the chart's dimensions on window resize
-    const handleResize = () => {
-      if (chart) {
-        chart.resize(containerRef.current.clientWidth, 800); // Adjust the height as needed
-      }
-    };
-
-    // Attach the resize event listener
-    window.addEventListener("resize", handleResize);
 
     return () => {
-      // Remove the resize event listener when the component unmounts
-      window.removeEventListener("resize", handleResize);
+      chartRef.current.remove(); // Clean up on component unmount
+    };
+  }, []);
 
-      if (chart) {
-        chart.remove();
+  // Update candles and chart with new WebSocket messages
+  useEffect(() => {
+    if (!lastMessage?.data) return;
+
+    const data = JSON.parse(lastMessage.data);
+    if (!data || !data.data) return;
+
+    const [timeStr, openStr, highStr, lowStr, closeStr] = data.data[0];
+    const newCandle = {
+      time: parseInt(timeStr, 10) / 1000, // Convert timestamp to seconds
+      open: parseFloat(openStr),
+      high: parseFloat(highStr),
+      low: parseFloat(lowStr),
+      close: parseFloat(closeStr),
+    };
+
+    const currentTime = Date.now() / 1000; // Current time in seconds
+    const tenMinutesAgo = currentTime - 600; // Time 10 minutes ago in seconds
+
+    // Filter out old candles and add the new one
+    const updatedCandles = candlesRef.current.filter(
+      (c) => c.time > tenMinutesAgo
+    );
+    const existingCandleIndex = updatedCandles.findIndex(
+      (c) => c.time === newCandle.time
+    );
+    if (existingCandleIndex >= 0) {
+      updatedCandles[existingCandleIndex] = newCandle; // Update existing candle
+    } else {
+      updatedCandles.push(newCandle); // Add new candle
+    }
+
+    candlesRef.current = updatedCandles; // Update the ref
+
+    candlestickSeriesRef.current.setData(updatedCandles); // Update the chart
+  }, [lastMessage?.data]);
+
+  // Handle window resize
+  useEffect(() => {
+    const handleResize = () => {
+      const currentChart = chartRef.current;
+      if (currentChart) {
+        currentChart.resize(containerRef.current.clientWidth, 800);
       }
     };
-  }, [parsedMessage]);
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return <div className="chart-container" ref={containerRef}></div>;
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,38 @@
 "use client";
 import CandlestickChart from "./components/candlestickChart";
 import AreaChart from "./components/areaChart";
-import React, { useEffect, useRef, useState } from "react";
-import useWebSocket from "react-use-websocket";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+// import useWebSocket from "react-use-websocket";
 
 export default function Home() {
-  const data = [
-    { value: 0, time: 1642425322 },
-    { value: 8, time: 1642511722 },
-    { value: 10, time: 1642598122 },
-    { value: 20, time: 1642684522 },
-    { value: 3, time: 1642770922 },
-    { value: 43, time: 1642857322 },
-    { value: 41, time: 1642943722 },
-    { value: 43, time: 1643030122 },
-    { value: 56, time: 1643116522 },
-    { value: 46, time: 1643202922 },
-  ];
-  const candlestickData = [
-    { time: 1672502400000, open: 100, high: 110, low: 90, close: 105 }, // Green candle (up trade)
-    { time: 1672588800000, open: 105, high: 115, low: 100, close: 112 }, // Green candle (up trade)
-    { time: 1672675200000, open: 112, high: 120, low: 105, close: 108 }, // Red candle (down trade)
-    { time: 1672761600000, open: 108, high: 115, low: 100, close: 104 }, // Red candle (down trade)
-    { time: 1672848000000, open: 104, high: 110, low: 95, close: 108 }, // Green candle (up trade)
-    { time: 1672934400000, open: 108, high: 115, low: 100, close: 112 }, // Green candle (up trade)
-    { time: 1673020800000, open: 112, high: 120, low: 105, close: 108 }, // Red candle (down trade)
-    { time: 1673107200000, open: 108, high: 115, low: 100, close: 104 }, // Red candle (down trade)
-    { time: 1673193600000, open: 104, high: 110, low: 95, close: 108 }, // Green candle (up trade)
-    { time: 1673280000000, open: 108, high: 115, low: 100, close: 112 }, // Green candle (up trade)
-    // Add more static candlestick data points as needed
-  ];
-  // WebSocket connection URL
-  const socketUrl = "wss://wspap.okx.com:8443/ws/v5/business?brokerId=9999";
+  // const socketUrl = "wss://wspap.okx.com:8443/ws/v5/business?brokerId=9999";
 
-  // The hook returns a send function and the last message received
-  const { sendMessage, lastMessage } = useWebSocket(socketUrl, {
-    onOpen: () => console.log("WebSocket Connected"),
-    // Will attempt to reconnect on all close events
-    shouldReconnect: (closeEvent) => true,
-  });
+  // const { sendMessage, lastMessage } = useWebSocket(socketUrl, {
+  //   onOpen: () => console.log("WebSocket Connected"),
+  //   shouldReconnect: (closeEvent) => true,
+  //   reconnectAttempts: 10,
+  //   reconnectInterval: 3000,
+  // });
 
+  // const handleMessage = useCallback(() => {
+  //   const message = {
+  //     op: "subscribe",
+  //     args: [
+  //       {
+  //         instId: "BTC-USD-SWAP",
+  //         channel: "mark-price-candle1m"
+  //       },
+  //     ],
+  //   };
+  //   sendMessage(JSON.stringify(message));
+  // }, [sendMessage]);
 
-  useEffect(() => {
-    const message = {
-      op: "subscribe",
-      args: [
-        {
-          channel: "mark-price-candle5m",
-          instId: "BTC-USD-SWAP",
-        },
-      ],
-    };
-    sendMessage(JSON.stringify(message));
-  }, [sendMessage]);
+  // useEffect(() => {
+  //   handleMessage();
+  // }, [handleMessage]);
 
-  // You can use lastMessage for the most recent message
-  // Use JSON.parse only when lastMessage is not null
-  const parsedMessage = lastMessage ? JSON.parse(lastMessage.data) : null;
-  // console.log(parsedMessage, "parsedMessage");
+  // const parsedMessage = lastMessage ? JSON.parse(lastMessage.data) : null;
+  // console.log(parsedMessage)
   return (
     <React.Fragment>
       <div className="w-full h-screen  p-8">
@@ -66,13 +41,10 @@ export default function Home() {
           <div className="w-full flex flex-col gap-3  shadow-md bg-slate-50 rounded-sm p-5">
             <span className="text-3xl text-black font-bold">Candle chart</span>
             <CandlestickChart 
-            parsedMessage={parsedMessage}
+            // parsedMessage={parsedMessage}
              />
           </div>
-          <div className="w-full flex flex-col gap-3  shadow-md bg-slate-50 rounded-sm p-5">
-            <span className="text-3xl text-black font-bold">Area chart</span>
-            <AreaChart parsedMessage={parsedMessage} />
-          </div>
+          
         </div>
       </div>
     </React.Fragment>
